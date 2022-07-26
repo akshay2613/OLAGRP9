3c8deff6cbef98f8a409e0ad072e2fbef80af1eb
apoorvlodhi-io <apoorvlodhi21@gmail.com>
2022-07-26 12:22:36 +0530

    Branch Init

--- backend/KhanaKhazana/src/main/java/com/khana/khazana/controller/UserController.java
+++ backend/KhanaKhazana/src/main/java/com/khana/khazana/controller/UserController.java
@@ -1,4 +1,15 @@
 package com.khana.khazana.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @GetMapping(value = "Home")
+    public String home(){
+        return "Hello friends khana khalo";
+    }
+
 }
